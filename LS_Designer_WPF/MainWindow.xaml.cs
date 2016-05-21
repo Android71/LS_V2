d9c8aa4d8e4425d636884612ddbc2be33b301523
Андрей <@AK>
2016-05-21 18:19:37 +0300

    Добавьте файлы проекта.

---
+++ LS_Designer_WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using LS_Designer_WPF.ViewModel;
+
+namespace LS_Designer_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
---
+++ LS_Designer_WPF/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace LS_Designer_WPF.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
