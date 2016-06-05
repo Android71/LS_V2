5cf2f189b52477ac707d8b56d9dac79f6a853043
Андрей <took77orders@list.ru>
2016-06-05 20:58:25 +0300

    333 0506

---
+++ LS_Designer_WPF/WPF_Extras/Extentions/WaterMarkExtention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LS_Designer_WPF.WPF_Extras
+{
+    public sealed class WaterMarkExtentions
+    {
+        public static string GetWaterMark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WaterMarkProperty);
+        }
+
+        public static void SetWaterMark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WaterMarkProperty, value);
+        }
+
+        public static readonly DependencyProperty WaterMarkProperty =
+
+           DependencyProperty.RegisterAttached("WaterMark"
+                                       , typeof(string)
+                                       , typeof(WaterMarkExtentions)
+                                       , new PropertyMetadata(""));
+
+    }
+
+}
