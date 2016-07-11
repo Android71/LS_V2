6fba138592960790c5955b289cc59d58f6b7d388
Андрей <@AK>
2016-07-11 21:43:09 +0300

    333 1107

--- LS_Designer_WPF/ViewModel/TabItemsVM/ScenesVM.cs
+++ LS_Designer_WPF/ViewModel/TabItemsVM/ScenesVM.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using LS_Designer_WPF.Model;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace LS_Designer_WPF.ViewModel
 { 
@@ -17,12 +19,157 @@ namespace LS_Designer_WPF.ViewModel
             _dataService = dataService;
         }
 
+        /*************************************************************/
+
+        #region Scene
+
+        #region Properties
+
         ObservableCollection<Scene> _sceneList;
-        ObservableCollection<Scene> SceneList
+        public ObservableCollection<Scene> SceneList
         {
             get { return _sceneList; }
             set { Set(ref _sceneList, value); }
         }
+
+        Scene _selectedScene;
+        public Scene SelectedScene
+        {
+            get { return _selectedScene; }
+            set { Set(ref _selectedScene, value); }
+        }
+
+        bool SceneAddMode { get; set; } = false;
+
+        bool SceneEditMode { get; set; } = false;
+
+        #endregion
+
+        #region Commands
+
+        #region Add Command
+
+        public RelayCommand SceneAddCmd { get; private set; }
+
+        void SceneExecAdd()
+        {
+            
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool SceneCanExecAdd()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #region Remove Command
+
+        public RelayCommand SceneRemoveCmd { get; private set; }
+
+        void SceneExecRemove()
+        {
+
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool SceneCanExecRemove()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #region Cancel Command
+
+        public RelayCommand SceneCancelCmd { get; private set; }
+
+        void SceneExecCancel()
+        {
+
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool SceneCanExecCancel()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #region Save Command
+
+        public RelayCommand SceneSaveCmd { get; private set; }
+
+        void SceneExecSave()
+        {
+
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool SceneCanExecSave()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #region Accent Add Command
+
+        public RelayCommand AccentAddCmd { get; private set; }
+
+        void AccentExecAdd()
+        {
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool AccentCanExecAdd()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #region Accent Remove Command
+
+        public RelayCommand AccentRemoveCmd { get; private set; }
+
+        void AccentExecRemove()
+        {
+            MessengerInstance.Send("", AppContext.BlockUIMsg);
+        }
+
+        bool AccentCanExecRemove()
+        {
+            return !SceneAddMode && !SceneEditMode;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Scene UI State
+
+        Visibility _sceneListCurtainVisibility = Visibility.Collapsed;
+        public Visibility SceneListCurtainVisibility
+        {
+            get { return _sceneListCurtainVisibility; }
+            set { Set(ref _sceneListCurtainVisibility, value); }
+        }
+
+        Visibility _sceneObjectButtonsVisibility = Visibility.Collapsed;
+        public Visibility SceneObjectButtonsVisibility
+        {
+            get { return _sceneObjectButtonsVisibility; }
+            set { Set(ref _sceneObjectButtonsVisibility, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        /*************************************************************/
 
         ObservableCollection<LightZone> _sceneZones;
         ObservableCollection<LightZone> SceneZones
