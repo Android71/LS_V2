c366692ae8caf11543bafac4074a72baf35b093d
Андрей <@AK>
2016-07-19 16:00:30 +0300

    111 1907

---
+++ CustomControls/SliderItem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LS_Designer_WPF.Controls
+{
+
+    public enum SliderVariant { Gradient, RangeLeftLimit, RangeRightLimit, Lightness };
+
+    public class SliderItem : Slider
+    {
+        static SliderItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderItem), new FrameworkPropertyMetadata(typeof(SliderItem)));
+        }
+
+        #region DP
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(SliderItem),
+            new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.None));
+
+        /// <summary>
+        /// Gets or sets a value that indicates if this slider is selected.
+        /// A slider remains selected until another silder is selected.
+        /// </summary>
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        // Variant = 0 GradientStop
+        // Variant = 1 Range Left Limit
+        // Variant = 2 Range Right Limit
+        // Variant = 3 Lightness control
+        public SliderVariant Variant
+        {
+            get { return (SliderVariant)GetValue(VariantProperty); }
+            set { SetValue(VariantProperty, value); }
+        }
+
+        public static readonly DependencyProperty VariantProperty =
+            DependencyProperty.Register("Variant", typeof(SliderVariant), typeof(SliderItem), new FrameworkPropertyMetadata(SliderVariant.Gradient));
+
+        #endregion
+
+        #region Properties
+
+        public int LeftLimit { get; set; }
+
+        #endregion
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            if (newValue >= SelectionStart && newValue <= SelectionEnd)
+                base.OnValueChanged(oldValue, newValue);
+            else
+            {
+                if (newValue < SelectionStart)
+                    Value = SelectionStart;
+                if (newValue > SelectionEnd)
+                    Value = SelectionEnd;
+            }
+        }
+    }
+}
---
+++ PatternEffect/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using PatternEffect.ViewModel;
+using LS_Designer_WPF.Controls;
+
+namespace PatternEffect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
