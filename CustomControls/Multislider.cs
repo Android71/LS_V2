c5daf778c3afb49072847b37ed2990da8a641c44
Андрей <@AK>
2016-07-26 15:46:56 +0300

    111 2607

--- CustomControls/Multislider.cs
+++ CustomControls/Multislider.cs
@@ -20,7 +20,6 @@ namespace LS_Designer_WPF.Controls
         Grid slidersArea;
         Border track;
 
-        SliderItem selectedSlider = null;
         int sliderIx = -1;
 
         #endregion
@@ -39,11 +38,8 @@ namespace LS_Designer_WPF.Controls
             DownSliders = Template.FindName("PART_DownSliders", this) as Grid;
             slidersArea.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(OnTrackClick);
             if (SliderList != null)
-                //UpSliders.Children.Add(SliderList[0]);
                 ReArrangeSliderItems();
         }
-
-        
 
         void ReArrangeSliderItems()
         {
@@ -72,6 +68,7 @@ namespace LS_Designer_WPF.Controls
 
         #region DP
 
+        #region SliderList
 
         public ObservableCollection<SliderItem> SliderList
         {
@@ -95,19 +92,16 @@ namespace LS_Designer_WPF.Controls
             //ms.UpSliders.Children.Add(ms.SliderList[0]);
         }
 
+        #endregion
 
-
-        public int SelectedMode
+        public int AddMode
         {
-            get { return (int)GetValue(SelectedModeProperty); }
-            set { SetValue(SelectedModeProperty, value); }
+            get { return (int)GetValue(AddModeProperty); }
+            set { SetValue(AddModeProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for SelectedMode.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty SelectedModeProperty =
-            DependencyProperty.Register("SelectedMode", typeof(int), typeof(MultiSlider), new PropertyMetadata(0));
-
-
+        public static readonly DependencyProperty AddModeProperty =
+            DependencyProperty.Register("AddMode", typeof(int), typeof(MultiSlider), new PropertyMetadata(0));
 
         public int Value
         {
@@ -115,11 +109,8 @@ namespace LS_Designer_WPF.Controls
             set { SetValue(ValueProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ValueProperty =
             DependencyProperty.Register("Value", typeof(int), typeof(MultiSlider), new PropertyMetadata(0));
-
-
 
         public PatternPoint[] Pattern
         {
@@ -136,20 +127,29 @@ namespace LS_Designer_WPF.Controls
             set { SetValue(MaxlimitProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Maxlimit.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty MaxlimitProperty =
             DependencyProperty.Register("Maxlimit", typeof(int), typeof(MultiSlider), new PropertyMetadata(0));
 
-
-
-        public ICommand UpdatePattern
+        public ICommand UpdatePatternCommand
         {
-            get { return (ICommand)GetValue(UpdatePatternProperty); }
-            set { SetValue(UpdatePatternProperty, value); }
+            get { return (ICommand)GetValue(UpdatePatternCommandProperty); }
+            set { SetValue(UpdatePatternCommandProperty, value); }
         }
 
-        public static readonly DependencyProperty UpdatePatternProperty =
-            DependencyProperty.Register("UpdatePattern", typeof(ICommand), typeof(MultiSlider), new PropertyMetadata(null));
+        public static readonly DependencyProperty UpdatePatternCommandProperty =
+            DependencyProperty.Register("UpdatePatternCommand", typeof(ICommand), typeof(MultiSlider), new PropertyMetadata(null));
+
+
+
+        public SliderItem SelectedSlider
+        {
+            get { return (SliderItem)GetValue(SelectedSliderProperty); }
+            set { SetValue(SelectedSliderProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedSliderProperty =
+            DependencyProperty.Register("SelectedSlider", typeof(SliderItem), typeof(MultiSlider), new PropertyMetadata(null));
+
 
         #endregion
 
@@ -159,23 +159,21 @@ namespace LS_Designer_WPF.Controls
 
         private void SliderItemGotMouseCapture(object sender, MouseEventArgs e)
         {
-            if (selectedSlider != null)
+            if (SelectedSlider != null)
             {
-                selectedSlider.IsSelected = false;
+                SelectedSlider.IsSelected = false;
                 
             }
             SliderItem si = sender as SliderItem;
-            //startMove = true;
-            //currentIx = 
             sliderIx = SliderList.IndexOf(si);
-            selectedSlider = si;
-            selectedSlider.IsSelected = true;
-            Value = (int)selectedSlider.Value;
+            SelectedSlider = si;
+            SelectedSlider.IsSelected = true;
+            Value = (int)SelectedSlider.Value;
             if (sliderIx == 0)
             {
                 // firtst slider in list
                 si.SelectionStart = 1;
-                if (selectedSlider.Variant == PointVariant.RangeLeft)
+                if (SelectedSlider.Variant == PointVariant.RangeLeft)
                     si.SelectionEnd = SliderList[sliderIx + 1].Value;
                 else
                     si.SelectionEnd = SliderList[sliderIx + 1].Value - 1;
@@ -186,7 +184,7 @@ namespace LS_Designer_WPF.Controls
             {
                 //last slider in list
                 si.SelectionEnd = Maxlimit;
-                if (selectedSlider.Variant == PointVariant.RangeRight)
+                if (SelectedSlider.Variant == PointVariant.RangeRight)
                     si.SelectionStart = SliderList[sliderIx - 1].Value;
                 else
                     si.SelectionStart = SliderList[sliderIx - 1].Value + 1;
@@ -194,21 +192,21 @@ namespace LS_Designer_WPF.Controls
             }
 
             // internal slider
-            if (selectedSlider.Variant == PointVariant.RangeRight)
+            if (SelectedSlider.Variant == PointVariant.RangeRight)
             {
                 si.SelectionStart = SliderList[sliderIx - 1].Value;
                 si.SelectionEnd = SliderList[sliderIx + 1].Value - 1;
                 return;
             }
 
-            if (selectedSlider.Variant == PointVariant.RangeLeft)
+            if (SelectedSlider.Variant == PointVariant.RangeLeft)
             {
                 si.SelectionStart = SliderList[sliderIx - 1].Value + 1;
                 si.SelectionEnd = SliderList[sliderIx + 1].Value;
                 return;
             }
 
-            if (selectedSlider.Variant != PointVariant.RangeLeft && selectedSlider.Variant != PointVariant.RangeRight)
+            if (SelectedSlider.Variant != PointVariant.RangeLeft && SelectedSlider.Variant != PointVariant.RangeRight)
             {
                 si.SelectionStart = SliderList[sliderIx - 1].Value + 1;
                 si.SelectionEnd = SliderList[sliderIx + 1].Value - 1;
@@ -233,8 +231,8 @@ namespace LS_Designer_WPF.Controls
                 pp.L = si.PatternPoint.L;
                 pp.UpdateColor();
                 si.PatternPoint = pp;
-                UpdatePattern.Execute(new SliderDuplet(SliderList[sliderIx - 1], si, true));
-                UpdatePattern.Execute(new SliderDuplet(si, SliderList[sliderIx + 1], true));
+                UpdatePatternCommand.Execute(new SliderDuplet(SliderList[sliderIx - 1], si, true));
+                UpdatePatternCommand.Execute(new SliderDuplet(si, SliderList[sliderIx + 1], true));
                 return;
             }
             else
@@ -245,15 +243,15 @@ namespace LS_Designer_WPF.Controls
             if (sliderIx == 0)
             {
                 // First slider movement
-                UpdatePattern.Execute(new SliderDuplet(null, si));
-                UpdatePattern.Execute(new SliderDuplet(si, SliderList[sliderIx + 1]));
+                UpdatePatternCommand.Execute(new SliderDuplet(null, si));
+                UpdatePatternCommand.Execute(new SliderDuplet(si, SliderList[sliderIx + 1]));
                 return;
             }
             if (sliderIx == SliderList.Count - 1)
             {
                 // Last slider movement
-                UpdatePattern.Execute(new SliderDuplet(si, null));
-                UpdatePattern.Execute(new SliderDuplet(SliderList[sliderIx - 1], si));
+                UpdatePatternCommand.Execute(new SliderDuplet(si, null));
+                UpdatePatternCommand.Execute(new SliderDuplet(SliderList[sliderIx - 1], si));
                 return;
             }
             //if (si.Variant == PointVariant.RangeLeft)
@@ -267,8 +265,8 @@ namespace LS_Designer_WPF.Controls
             //    return;
             //}
 
-            UpdatePattern.Execute(new SliderDuplet(SliderList[sliderIx - 1], si));
-            UpdatePattern.Execute(new SliderDuplet(si, SliderList[sliderIx + 1]));
+            UpdatePatternCommand.Execute(new SliderDuplet(SliderList[sliderIx - 1], si));
+            UpdatePatternCommand.Execute(new SliderDuplet(si, SliderList[sliderIx + 1]));
 
             //si.Busy = false;
 
@@ -310,15 +308,210 @@ namespace LS_Designer_WPF.Controls
 
         private void OnTrackClick(object sender, MouseButtonEventArgs e)
         {
-            System.Windows.Point pt = e.GetPosition((UIElement)sender);
-            double halfStep = (slidersArea.ActualWidth + 2 * this.Margin.Left) / this.Maxlimit;
-            int siderPos = Convert.ToInt32(pt.X / halfStep);
-            Console.WriteLine($"ActualWidth + Margin {slidersArea.ActualWidth + 2 * this.Margin.Left}");
-            Console.WriteLine($"ptX {pt.X}");
-            Console.WriteLine($"LedPos: {siderPos}");
-            Console.WriteLine($"CurrentMode: {SelectedMode}");
-            if (siderPos < 1)
-                siderPos = 1;
+            if (e.ClickCount == 2)      // DoubleClick
+            {
+                System.Windows.Point pt = e.GetPosition((UIElement)sender);
+                double halfStep = (slidersArea.ActualWidth + 2 * Margin.Left) / this.Maxlimit;
+                int sliderPos = Convert.ToInt32(pt.X / halfStep) + 1;
+                Console.WriteLine($"ActualWidth + Margin {slidersArea.ActualWidth + 2 * this.Margin.Left}");
+                Console.WriteLine($"ptX {pt.X}");
+                Console.WriteLine($"LedPos: {sliderPos}");
+                Console.WriteLine($"CurrentMode: {AddMode}");
+
+                // AddMode = 0 Gradient
+                // AddMode = 1 Range
+                // AddMode = 2 Lightness
+
+                PatternPoint pp = Pattern[sliderPos - 1];
+
+                // если первая точка в Pattern
+                if (sliderPos < SliderList[0].Value)
+                {
+                    SliderItem tmpSi = SliderList[0];
+                    switch (AddMode)
+                    {
+                        case 1:   //Range
+                            SliderList[0].PatternPoint.CopyTo(pp);
+                            SliderItem rangeLeft = CreateSlider(sliderPos, Maxlimit, pp);
+                            rangeLeft.Variant = PointVariant.RangeLeft;
+                            SliderItem rangeRight = CreateSlider(sliderPos, Maxlimit, pp);
+                            rangeRight.Variant = PointVariant.RangeRight;
+                            SliderList.Insert(0, rangeRight);
+                            SliderList.Insert(0, rangeLeft);
+                            ReArrangeSliderItems();
+
+                            //UpdatePatternCommand.Execute(new SliderDuplet(rangeLeft, rangeRight));
+                            UpdatePatternCommand.Execute(new SliderDuplet(rangeRight, tmpSi));
+                            break;
+                        case 2:     //Lightness
+                            break;
+                        default:    //Gradient        
+                            SliderList[0].PatternPoint.CopyTo(pp);
+                            SliderItem gradient = CreateSlider(sliderPos, Maxlimit, pp);
+                            SliderList.Insert(0, gradient);
+                            ReArrangeSliderItems();
+
+                            UpdatePatternCommand.Execute(new SliderDuplet(gradient, tmpSi));
+                            break;
+                    }
+                    return;
+                }
+
+                // если последняя точка в Pattern
+                if (sliderPos > SliderList[SliderList.Count - 1].Value)
+                {
+                    SliderItem lastSi = SliderList[SliderList.Count - 1];
+                    switch (AddMode)
+                    {
+                        case 1:     // Range
+                            SliderList[SliderList.Count - 1].PatternPoint.CopyTo(pp);
+                            SliderItem rangeLeft = CreateSlider(sliderPos, Maxlimit, pp);
+                            rangeLeft.Variant = PointVariant.RangeLeft;
+                            SliderItem rangeRight = CreateSlider(sliderPos, Maxlimit, pp);
+                            rangeRight.Variant = PointVariant.RangeRight;
+                            SliderList.Add(rangeLeft);
+                            SliderList.Add(rangeRight);
+                            ReArrangeSliderItems();
+
+                            UpdatePatternCommand.Execute(new SliderDuplet(lastSi, rangeLeft));
+                            //UpdatePatternCommand.Execute(new SliderDuplet(rangeLeft, rangeRight));
+                            break;
+                        case 2:     //Lightness
+                            break;
+                        default:    //Gradient
+                            SliderList[SliderList.Count - 1].PatternPoint.CopyTo(pp);
+                            SliderItem gradient = CreateSlider(sliderPos, Maxlimit, pp);
+                            SliderList.Add(gradient);
+                            ReArrangeSliderItems();
+
+                            UpdatePatternCommand.Execute(new SliderDuplet(lastSi, gradient));
+
+                            break;
+                    }
+                    return;
+                }
+
+                // между PatternPoint
+                SliderItem prevSi = null;
+                SliderItem nextSi = null;
+                int minLeft = Maxlimit;
+                int minRight = Maxlimit;
+                foreach(SliderItem si in SliderList)
+                {
+                    if (si.Value > sliderPos)
+                    {
+                        if ((si.Value - sliderPos)< minRight)
+                        {
+                            minRight = (int)si.Value - sliderPos;
+                            nextSi = si;
+                        }
+                    }
+
+                    if (si.Value < sliderPos)
+                    {
+                        if ((sliderPos - si.Value) < minLeft)
+                        {
+                            minLeft = sliderPos - (int)si.Value;
+                            prevSi = si;
+                        }
+                    }
+                }
+                if (nextSi.Variant == PointVariant.RangeRight)
+                    // нельзя добавить точку внутри Range
+                    return;
+                SliderItem newSlider = CreateSlider(sliderPos, Maxlimit, Pattern[sliderPos - 1]);
+                int ix = SliderList.IndexOf(nextSi);
+                if (AddMode == 1)
+                {
+                    newSlider.Variant = PointVariant.RangeLeft;
+                    SliderItem rangeRight = CreateSlider(sliderPos, Maxlimit, Pattern[sliderPos - 1]);
+                    rangeRight.Variant = PointVariant.RangeRight;
+                    SliderList.Insert(ix, rangeRight);
+                    SliderList.Insert(ix, newSlider);
+                    ReArrangeSliderItems();
+                    return;
+                }
+                if (AddMode == 2)
+                    newSlider.Variant = PointVariant.Lightness;
+                SliderList.Insert(ix, newSlider);
+                ReArrangeSliderItems();
+            }
+        }
+
+        SliderItem CreateSlider(int value, int max, PatternPoint point)
+        {
+            SliderItem si = new SliderItem();
+            si.PatternPoint = point;
+            si.Minimum = 1;
+            si.Maximum = max;
+            si.SelectionStart = 1;
+            si.SelectionEnd = max;
+            si.Value = value;
+            return si;
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (SelectedSlider != null)
+            {
+                //Console.WriteLine($"Delta: {e.Delta}");
+                if (e.Delta > 0)
+                {
+                    SelectedSlider.PatternPoint.L += 0.02;
+                    if (SelectedSlider.PatternPoint.L > 1.0)
+                        SelectedSlider.PatternPoint.L = 1.0;
+                }
+                else
+                {
+                    SelectedSlider.PatternPoint.L -= 0.02;
+                    if (SelectedSlider.PatternPoint.L < 0.0)
+                        SelectedSlider.PatternPoint.L = 0.0;
+                }
+            }
+            SelectedSlider.PatternPoint.UpdateColor();
+            UpdatePattern(SelectedSlider);
+        }
+
+        void UpdatePattern(SliderItem currentSlider)
+        {
+            SliderItem prevSlider = null;
+            SliderItem nextSlider = null;
+            SliderItem afterRangeRight = null;
+            SliderItem beforeRangeLeft = null;
+
+            if (currentSlider != null)
+            {
+                int ix = SliderList.IndexOf(currentSlider);
+                if (ix != 0)
+                    prevSlider = SliderList[ix - 1];
+                if (ix != SliderList.Count - 1)
+                    nextSlider = SliderList[ix + 1];
+
+                if (currentSlider.Variant == PointVariant.RangeLeft)
+                {
+                    currentSlider.PatternPoint.CopyTo(nextSlider.PatternPoint);
+                    if ((ix + 1) != (SliderList.Count -1))
+                    {
+                        // Slider RangeRight не последний Slider в SliderList
+                        afterRangeRight = SliderList[ix + 2];
+                        UpdatePatternCommand.Execute(new SliderDuplet(nextSlider, afterRangeRight));
+                    }
+                }
+                if (currentSlider.Variant == PointVariant.RangeRight)
+                {
+                    currentSlider.PatternPoint.CopyTo(prevSlider.PatternPoint);
+                    if ((ix - 1)!= 0)
+                    {
+                        // Slider RangeLeft не первый Slider в SliderList
+                        beforeRangeLeft = SliderList[ix - 2];
+                        UpdatePatternCommand.Execute(new SliderDuplet(beforeRangeLeft, prevSlider));
+                    }
+                }
+
+                UpdatePatternCommand.Execute(new SliderDuplet(prevSlider, currentSlider));
+                UpdatePatternCommand.Execute(new SliderDuplet(currentSlider, nextSlider));
+            }
         }
 
         #endregion
